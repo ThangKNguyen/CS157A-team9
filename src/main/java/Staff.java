ec666b719158cb07d458961b99e63b8769566754
tyebii <tyler.biesemeyer2022@gmail.com>
2024-10-15 22:16:15 -0700

    staff table/page

---
+++ src/main/java/Staff.java
@@ -0,0 +1,48 @@
+
+public class Staff {
+	private String name,email,phone,address;
+
+	public Staff() {
+		super();
+	}
+	
+	public Staff(String name, String email, String phone, String address) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
---
+++ src/main/java/StaffRegister.java
@@ -0,0 +1,48 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StaffRegister
+ */
+@WebServlet("/StaffRegister")
+public class StaffRegister extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StaffRegister() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name=request.getParameter("name");
+		String email=request.getParameter("email");
+		String phone=request.getParameter("phone");
+		String address=request.getParameter("address");
+		
+		Staff staff = new Staff(name, email, phone, address);
+		StaffRegisterDao srDao = new StaffRegisterDao();
+		String result = srDao.insert(staff);
+		response.getWriter().println(result);
+	}
+
+}
