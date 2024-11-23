f2aae7c13286028ff62582c91d1e1ddd1759e2d1
andresaclan <aclanandres@gmail.com>
2024-11-23 14:58:31 -0800

    book return

--- src/main/java/com/LibTrack/dao/BookDao.java
+++ src/main/java/com/LibTrack/dao/BookDao.java
@@ -132,38 +132,59 @@ public class BookDao {
 
 		return books;
 	}
-	
+
 	public List<BookDetails> getAvailableBooksWithDetails() {
-	    List<BookDetails> books = new ArrayList<>();
-	    String query = "SELECT Books.BookID, Books.Title, Books.ISBN, Authors.Name AS AuthorName, "
-	            + "Categories.CategoryName, Genres.GenreName, Books.Availability "
-	            + "FROM LibTrack.Books "
-	            + "JOIN LibTrack.Authors ON Books.AuthorID = Authors.AuthorID "
-	            + "JOIN LibTrack.Categories ON Books.CategoryID = Categories.CategoryID "
-	            + "JOIN LibTrack.Genres ON Books.GenreID = Genres.GenreID "
-	            + "WHERE Books.Availability = 'Available'";
+		List<BookDetails> books = new ArrayList<>();
+		String query = "SELECT Books.BookID, Books.Title, Books.ISBN, Authors.Name AS AuthorName, "
+				+ "Categories.CategoryName, Genres.GenreName, Books.Availability " + "FROM LibTrack.Books "
+				+ "JOIN LibTrack.Authors ON Books.AuthorID = Authors.AuthorID "
+				+ "JOIN LibTrack.Categories ON Books.CategoryID = Categories.CategoryID "
+				+ "JOIN LibTrack.Genres ON Books.GenreID = Genres.GenreID " + "WHERE Books.Availability = 'Available'";
 
-	    try (Connection con = DatabaseConn.getConnection();
-	         PreparedStatement ps = con.prepareStatement(query);
-	         ResultSet rs = ps.executeQuery()) {
+		try (Connection con = DatabaseConn.getConnection();
+				PreparedStatement ps = con.prepareStatement(query);
+				ResultSet rs = ps.executeQuery()) {
 
-	        while (rs.next()) {
-	            BookDetails book = new BookDetails();
-	            book.setBookId(rs.getInt("BookID"));
-	            book.setTitle(rs.getString("Title"));
-	            book.setISBN(rs.getString("ISBN"));
-	            book.setAuthor(rs.getString("AuthorName"));
-	            book.setCategory(rs.getString("CategoryName"));
-	            book.setGenre(rs.getString("GenreName"));
-	            book.setAvailability(rs.getString("Availability"));
-	            books.add(book);
-	        }
+			while (rs.next()) {
+				BookDetails book = new BookDetails();
+				book.setBookId(rs.getInt("BookID"));
+				book.setTitle(rs.getString("Title"));
+				book.setISBN(rs.getString("ISBN"));
+				book.setAuthor(rs.getString("AuthorName"));
+				book.setCategory(rs.getString("CategoryName"));
+				book.setGenre(rs.getString("GenreName"));
+				book.setAvailability(rs.getString("Availability"));
+				books.add(book);
+			}
 
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-	    return books;
+		return books;
+	}
+
+	public boolean updateBookAvailability(int bookId) {
+		String query = "UPDATE LibTrack.Books SET Availability = 'Available' WHERE BookID = ?";
+
+		try (Connection con = DatabaseConn.getConnection(); PreparedStatement ps = con.prepareStatement(query)) {
+
+			// Set the bookId parameter in the query
+			ps.setInt(1, bookId);
+
+			// Execute the update query
+			int rowsAffected = ps.executeUpdate();
+
+			// If at least one row was updated, return true
+			if (rowsAffected > 0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// Return false if the update failed
+		return false;
 	}
 
 }
--- src/main/java/com/LibTrack/dao/BorrowingHistoryDao.java
+++ src/main/java/com/LibTrack/dao/BorrowingHistoryDao.java
@@ -12,7 +12,7 @@ import com.LibTrack.utils.DatabaseConn;
 
 public class BorrowingHistoryDao {
 	public List<BorrowingHistoryItem> getBorrowingHistoryByMemberId(int memberId) {
-		String query = "SELECT bh.BorrowID, b.Title, bh.BorrowDate, bh.DueDate, bh.ReturnDate, bh.Status "
+		String query = "SELECT bh.BorrowID, b.BookID, b.Title, bh.BorrowDate, bh.DueDate, bh.ReturnDate, bh.Status "
 				+ "FROM LibTrack.Borrowing_History bh " + "JOIN Books b ON bh.BookID = b.BookID "
 				+ "WHERE bh.MemberID = ?";
 		List<BorrowingHistoryItem> borrowingHistory = new ArrayList<>();
@@ -21,6 +21,7 @@ public class BorrowingHistoryDao {
 			try (ResultSet rs = ps.executeQuery()) {
 				while (rs.next()) {
 					int borrowId = rs.getInt("BorrowID");
+					int bookId = rs.getInt("BookID");
 					String title = rs.getString("Title");
 					String borrowDate = rs.getDate("BorrowDate").toString();
 					String dueDate = rs.getDate("DueDate").toString();
@@ -33,7 +34,7 @@ public class BorrowingHistoryDao {
 					}
 					String status = rs.getString("Status");
 
-					BorrowingHistoryItem item = new BorrowingHistoryItem(borrowId, title, borrowDate, dueDate,
+					BorrowingHistoryItem item = new BorrowingHistoryItem(borrowId, bookId, title, borrowDate, dueDate,
 							returnDate, status);
 					borrowingHistory.add(item);
 				}
@@ -58,4 +59,28 @@ public class BorrowingHistoryDao {
 		}
 		return -1;
 	}
+
+	public boolean markBookAsReturned(int borrowId) {
+		String query = "UPDATE LibTrack.Borrowing_History SET Status = 'Returned', ReturnDate = CURRENT_DATE WHERE BorrowID = ?";
+
+		try (Connection con = DatabaseConn.getConnection(); PreparedStatement ps = con.prepareStatement(query)) {
+
+			// Set the borrowId parameter in the query
+			ps.setInt(1, borrowId);
+
+			// Execute the update query
+			int rowsAffected = ps.executeUpdate();
+
+			// If at least one row was updated, return true
+			if (rowsAffected > 0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// Return false if the update failed
+		return false;
+	}
+
 }
